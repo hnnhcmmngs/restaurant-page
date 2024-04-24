5d93429028f365ab2efe21f202a792cbd203fa4d
hnnhcmmngs <hannahcummings94@gmail.com>
2024-04-23 20:43:52 -0400

    Style header and footer
    The header and footer have been styled to include font sizes, font
    colors, backgrounds, padding, and border animations for the header
    buttons on hover.

--- src/home.js
+++ src/home.js
@@ -2,6 +2,8 @@ export default function loadHome() {
     const content = document.querySelector("#content");
 
     let intro = document.createElement("div");
+    let line1 = document.createElement("p");
+    let line2 = document.createElement("p");
     intro.textContent = "Welcome to Our Resutaurant. A new kind of restaurant.";
     intro.classList = "intro";
 
