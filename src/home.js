92bebcfb5354bebca0d597d4f5d0f448b878dc8e
hnnhcmmngs <hannahcummings94@gmail.com>
2024-04-24 20:49:28 -0400

    Style home page
    The content of the home page has been styled, including font size,
    font colors, backgrounds, spacing, and centering.

--- src/home.js
+++ src/home.js
@@ -3,9 +3,14 @@ export default function loadHome() {
 
     let intro = document.createElement("div");
     let line1 = document.createElement("p");
+    line1.innerHTML = "Welcome to <em>Our Restaurant</em>.";
+    line1.setAttribute("id", "line1");
     let line2 = document.createElement("p");
-    intro.textContent = "Welcome to Our Resutaurant. A new kind of restaurant.";
+    line2.textContent = "A new kind of restaurant.";
+    line2.setAttribute("id", "line2");
     intro.classList = "intro";
+    intro.appendChild(line1);
+    intro.appendChild(line2);
 
     content.appendChild(intro);
 }
